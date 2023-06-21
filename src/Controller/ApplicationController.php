26917f4fd15927bac97e106e0952fa78275e2d1a
Joanna Pathenay <j.pathenay@gmail.com>
2023-06-21 16:15:39 +0200

    F5-Affichage des chiens de l'annonce uniquement

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -7,6 +7,7 @@ use App\Entity\Offer;
 use App\Entity\Application;
 use App\Form\ApplicationFormType;
 use App\Repository\OfferRepository;
+use App\Repository\ApplicationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,15 +15,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApplicationController extends AbstractController
 {
     #[Route('/repondre-annonce-{id}', name: 'application_make', requirements: ['id' => '\d+'])]
-    public function makeApplication(OfferRepository $offerRepository, Offer $offer): Response
+    public function makeApplication(OfferRepository $offerRepository, Offer $offer, ApplicationRepository $applicationRepository): Response
     {
         $user = $this->getUser();
-        $message = new Message();
+        $message = (new Message())->setIsSentByAdopter(true);
         $application = (new Application())
             ->setUser($user)
             ->setOffer($offer)
             ->addMessage($message);
         $form = $this->createForm(ApplicationFormType::class, $application);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $applicationRepository->save($application, true);
+            $this->addFlash('success', 'Demande de renseignement envoyée');
+            // return $this->redirectToRoute('app_default');
+        }
+
         return $this->render('application/application_make.html.twig', [
             'offer' => $offer,
             'form' => $form->createView(),
--- src/Form/ApplicationFormType.php
+++ src/Form/ApplicationFormType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Application;
 use App\Entity\Dog;
 use App\Entity\Message;
+use App\Entity\Offer;
+use App\Repository\DogRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
@@ -15,9 +17,20 @@ class ApplicationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $offer = $options['data']->getOffer();
         $builder
             ->add('dogs', EntityType::class, [
                 'class' => Dog::class,
+                'query_builder' => function (DogRepository $request) use ($offer) {
+                    return $request->createQueryBuilder('d')
+                        ->leftJoin('d.offer', 'o')
+                        ->andWhere('d.isAdopted = false')
+                        ->andWhere('o.id = :id')
+                        ->setParameter('id', $offer->getId())
+                        ->orderBy('d.name', 'ASC');
+                },
+                'choice_label' => 'name',
+                'required' => false,
                 'label' => 'Chiens',
                 'multiple' => true,
                 'expanded' => true,

af7f508d5b71b99e77716b89edaf9bf4391c84b9
parya <antoine020999@gmail.com>
2023-06-26 14:15:49 +0200

    d3-affichage-conversation : modification controler + vue

--- src/Controller/ConversationController.php
+++ src/Controller/ConversationController.php
@@ -1,17 +1,18 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Application;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConversationController extends AbstractController
 {
-    #[Route('/conversation', name: 'app_conversation')]
-    public function index(): Response
+    #[Route('/conversation-{id}', name: 'app_conversation')]
+    public function index(Application $application): Response
     {
         return $this->render('conversation/index.html.twig', [
-            'controller_name' => 'ConversationController',
+            'application' => $application,
         ]);
     }
 }
